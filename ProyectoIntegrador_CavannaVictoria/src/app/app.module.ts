0c74561eea45f9d054d32eb245f4f9a7892e1046
viccavanna <84188451+viccavanna@users.noreply.github.com>
2023-01-05 21:40:17 -0300

    Avance FrontEnd

--- ProyectoIntegrador_CavannaVictoria/src/app/app.module.ts
+++ ProyectoIntegrador_CavannaVictoria/src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { SkillsComponent } from './components/skills/skills.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    SkillsComponent
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

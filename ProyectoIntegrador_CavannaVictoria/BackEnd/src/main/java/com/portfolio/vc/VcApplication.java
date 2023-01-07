4cd25caa4dfdb9f1a365a3a3a0350d3b3cdcf017
viccavanna <84188451+viccavanna@users.noreply.github.com>
2023-01-07 20:39:53 -0300

    FrontEnd Avance

---
+++ ProyectoIntegrador_CavannaVictoria/BackEnd/src/main/java/com/portfolio/vc/VcApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.vc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcApplication.class, args);
+	}
+
+}
